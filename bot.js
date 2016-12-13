4db578bd52d59beb8810d151db38dddeb15bc024
Alex Kiura <kiuraalex@gmail.com>
2016-12-13 18:18:20 +0300

    feat(retweet): refactor retweet

--- bot.js
+++ bot.js
@@ -1,28 +1,37 @@
 const twit = require('twit');
-const config = require('config');
+require('dotenv').config({ silent: true });
+
+const config = require('./config.js');
 
 const Twitter = twit(config);
 
+const retweeted = [];
 const retweet = () => {
  const params = {
-   q: '#nodejs, #Nodejs',
+   q: '#kangaroo',
    result_type: 'recent',
    lang: 'en',
  };
 
  Twitter.get('search/tweets', params, (err, data) =>{
    if (!err) {
+     const numOfTweets = data.statuses.length;
      let retweetId = data.statuses[0].id_str;
-     Twitter.post('statuses/retweet/:id',
-      { id: retweetId }, (err, response) => {
-        if (response) {
-          console.log('Retweeted!!');
-        }
 
-        if (err) {
-          console.log('Oops. Something went wrong while retweeting :(');
-        }
-     });
+      // retweet all tweets found
+      for (let i=0; i < numOfTweets; i++) {
+         Twitter.post('statuses/retweet/:id',
+          { id: data.statuses[i].id_str }, (err, response) => {
+            if (response.text) {
+              console.log('Retweeted!!', response.text);
+            }
+            if (err) {
+              console.log(err.message);
+              return;
+            }
+         });
+      }
+      console.log('finished retweeting. Bye!');
    }
    else {
      console.log('Oops. Something went wrong while searching :(');
@@ -34,6 +43,3 @@ const retweet = () => {
 
 // get retweeting once program is running
 retweet();
-
-// retweet every 50 minutes
-setInteval(retweet, 3000000);
